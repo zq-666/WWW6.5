ad8e1bc97b65ced7a96427354d44c059d09100d9
zq-666 <60081221+zq-666@users.noreply.github.com>
2026-03-04 14:31:24 +0000

    day2_SaveMyName

---
+++ zq/day2_SaveMyName.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Day2 {
+    string name;
+    string bio;
+
+    function add(string memory _name, string memory _bio) public {
+        name = _name;
+        bio = _bio;
+    }
+
+    function retrieve() public view returns (string memory, string memory) {
+        return (name, bio);
+    }
+}
