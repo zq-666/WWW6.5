02922c3e567ae598b9062fec920e7cf363a3bb82
zq-666 <60081221+zq-666@users.noreply.github.com>
2026-03-04 14:35:41 +0000

    day3_PollStation

---
+++ zq/day3_PollStation.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract PollStation{
+    string[] public candidateNames;
+    mapping (string => uint256) voteCount;
+    function addcandidateNames(string memory _candidateNames) public {
+        candidateNames.push(_candidateNames);
+        voteCount[_candidateNames] = 0;
+    }
+    function getcandidateNames() public view  returns (string[] memory) {
+        return  candidateNames;
+    }
+    function vote(string memory _candidateNames) public {
+        voteCount[_candidateNames] +=1;
+    }
+    function getVote(string memory _candidateNames) public view returns (uint256) {
+        return voteCount[_candidateNames];
+    }
+}
